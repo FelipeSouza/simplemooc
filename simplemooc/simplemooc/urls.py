92a27d87d26d900f5a04981dbeddc56c24e5b2b2
FelipeSouza <fso.martins@gmail.com>
2018-02-19 16:59:43 -0300

    Registando as urls, criandos as views e a pagina de contato

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path ('', include('simplemooc.core.urls')),
+    path ('contato/', include('simplemooc.core.urls')),
     path('admin/', admin.site.urls),
 ]
