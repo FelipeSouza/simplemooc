857af18e6ace229941b27cd92286cc72a6c31c84
FelipeSouza <fso.martins@gmail.com>
2018-02-19 21:14:21 -0300

    Criando os models

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    nome = models.CharField('Nome', max_length=200)
+    slug = models.SlugField('Atalho')
+    description = models.TextField('Descrição', blank=True)
+    star_date = models.DateField('Data início', null=True, blank=True)
+    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem')
+
+    created_at = models.DateTimeField('Criado em',auto_now_add=True)
+    updated_at = models.DateTimeField('Atualizado em', auto_now=True)
