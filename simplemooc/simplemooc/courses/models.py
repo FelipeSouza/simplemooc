5cc15cf22ad7332dd3efea4eac80670968313784
FelipeSouza <fso.martins@gmail.com>
2018-02-21 14:19:01 -0300

    Ajustando os campos no model

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -3,14 +3,15 @@ from django.db import models
 class CourseManager(models.Manager):
 
     def search(self, query):
-        return self.get_queryset(),filter(
-            name__icontains=query, description__icontains=query
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query)| \
+            models.Q(description__icontains=query)
         )
 
 
 class Course(models.Model):
 
-    nome = models.CharField('Nome', max_length=200)
+    name = models.CharField('Nome', max_length=200)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
     star_date = models.DateField('Data início', null=True, blank=True)
@@ -22,4 +23,4 @@ class Course(models.Model):
     created_at = models.DateTimeField('Criado em',auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
-objects = CourseManager()
+    objects = CourseManager()
