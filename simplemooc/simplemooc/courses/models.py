5f1ceb40c1889c04bb77e302424d19edf2c880b6
FelipeSouza <fso.martins@gmail.com>
2018-02-21 13:52:27 -0300

    Adicionando um object proprio e implementando a  def search

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset(),filter(
+            name__icontains=query, description__icontains=query
+        )
 
 
 class Course(models.Model):
+
     nome = models.CharField('Nome', max_length=200)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
@@ -14,3 +21,5 @@ class Course(models.Model):
 
     created_at = models.DateTimeField('Criado em',auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+objects = CourseManager()
