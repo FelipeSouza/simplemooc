6e360f6900f33a75b6e46a836c7c2578c2c693fa
FelipeSouza <fso.martins@gmail.com>
2018-02-21 16:52:32 -0300

    correcao no models e implementação de um custom admin

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -14,7 +14,7 @@ class Course(models.Model):
     name = models.CharField('Nome', max_length=200)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
-    star_date = models.DateField('Data início', null=True, blank=True)
+    start_date = models.DateField('Data início', null=True, blank=True)
     image = models.ImageField(
         upload_to='courses/images', verbose_name='Imagem',
         null=True, blank=True
@@ -24,3 +24,10 @@ class Course(models.Model):
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
     objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering =['name']
