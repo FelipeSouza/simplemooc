0e1a7c04a22a5d392719fc4961457aa2a7b56a10
FelipeSouza <fso.martins@gmail.com>
2018-02-21 13:45:03 -0300

    Editando o model

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+
+
 class Course(models.Model):
     nome = models.CharField('Nome', max_length=200)
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
     star_date = models.DateField('Data início', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem')
+    image = models.ImageField(
+        upload_to='courses/images', verbose_name='Imagem',
+        null=True, blank=True
+    )
 
     created_at = models.DateTimeField('Criado em',auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
