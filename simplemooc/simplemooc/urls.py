ea32528eadd84cd22c0c3214f3ed05e98629561e
FelipeSouza <fso.martins@gmail.com>
2018-02-21 17:50:11 -0300

    Criação das urls e views da app courses e implementação dos templates

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path ('', include('simplemooc.core.urls')),
+    path ('', include ('simplemooc.courses.urls')),
     path('admin/', admin.site.urls),
 ]
